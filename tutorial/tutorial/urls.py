b48a17980b6701a3ed9a95264e199bfcf1035589
nicoman <nicolasmanso@gmail.com>
2015-02-06 08:49:48 -0300

    Add routing

--- tutorial/tutorial/urls.py
+++ tutorial/tutorial/urls.py
@@ -1,8 +1,12 @@
-from django.conf.urls import patterns, include, url
-from django.contrib import admin
+from snippets import views
+from django.conf.urls import patterns, url, include
+from rest_framework.routers import DefaultRouter
+
+router = DefaultRouter()
+router.register(r'snippets', views.SnippetViewSet)
+router.register(r'users', views.UserViewSet)
 
 urlpatterns = patterns('',
-    url(r'^admin/', include(admin.site.urls)),
-    url(r'', include('snippets.urls')),
-    url(r'^api-auth/', include('rest_framework.urls', namespace='rest_framework')),
+    url(r'^', include(router.urls)),
+    url(r'^api-auth/', include('rest_framework.urls', namespace='rest_framework'))
 )
